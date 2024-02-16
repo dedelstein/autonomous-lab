c531fbb759902e5558a7de7073b4a8db8724098d
Dan <danhedelstein@gmail.com>
2024-02-16 16:33:29 +0200

    still trying to get readthedocs going

---
+++ docs/conf.py
@@ -0,0 +1,9 @@
+# Bare minimum conf.py file
+
+project = 'Autonomous Lab Bench'
+author = 'Dan Edelstein'
+extensions = []
+templates_path = ['_templates']
+exclude_patterns = []
+html_theme = 'alabaster'
+html_static_path = ['_static']
