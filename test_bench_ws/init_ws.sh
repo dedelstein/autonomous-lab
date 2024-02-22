09e1cd1cb78d4221c1a0509e662f97de25ef2606
Dan <danhedelstein@gmail.com>
2024-02-22 15:26:11 +0200

    Workplace init

---
+++ test_bench_ws/init_ws.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+# This script is used to initialize the workspace for the first time
+
+git submodule update --init && \
+colcon build --packages-select libsick_ldmrs --event-handlers console_direct+ && \
+source ./install/setup.bash && \
+colcon build --packages-select sick_scan_xd --cmake-args " -DROS_VERSION=2" --event-handlers console_direct+ && \
+source ./install/setup.bash && \
+colcon build --packages-select test_bench && \
+source ./install/setup.bash
