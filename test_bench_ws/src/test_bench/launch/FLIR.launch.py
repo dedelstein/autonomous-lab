ade9fff2434f9b58bf6de99f800ea7614e14e2ec
Dan <danhedelstein@gmail.com>
2024-02-22 12:54:40 +0200

    working on FLIR

--- test_bench_ws/src/test_bench/launch/FLIR.launch.py
+++ test_bench_ws/src/test_bench/launch/FLIR.launch.py
@@ -2,6 +2,10 @@ import os
 
 from launch_ros.actions import Node
 from launch import LaunchDescription
+from ament_index_python.packages import get_package_share_directory
+
+pkg_path = os.path.join(get_package_share_directory('test_bench'))
+FLIR_params = os.path.join(pkg_path,'config','FLIR_camera.yaml')
 
 def generate_launch_description():
 
@@ -10,11 +14,12 @@ def generate_launch_description():
         executable = "camera_driver_node",
         output='screen',
         namespace='camera',
-        parameters={
+        parameters=[{
+            'parameter_file': FLIR_params,
             'camera_type': 'chameleon',
             'serial_number': '20073275',
             'frame_id': 'FLIR'
-            }
+            }]
         )
     return LaunchDescription([
         flir_node
