b958dd8f19b09c94799b713d5619bed5910c2c4f
Autonomous Vehicle Lab <danhedelstein@gmail.com>
2024-04-09 18:09:52 +0300

    urdf and marvelmind

--- ROS2_Master/test_bench_ws/init_ws.sh
+++ ROS2_Master/test_bench_ws/init_ws.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
 # This script is used to initialize the workspace for the first time
+# NB libsick_ldmrs must be built and sourced before sick_scan otherwise we could just do colcon build --cmake-args "-DROS_VERSION=2".
+# Someone could probably fix this.  someone else.
 
 colcon build --packages-select libsick_ldmrs --event-handlers console_direct+ && \
 source /opt/ros/iron/setup.bash && \
@@ -8,4 +10,7 @@ source ./install/setup.bash && \
 colcon build --packages-select sick_scan_xd --cmake-args " -DROS_VERSION=2" --event-handlers console_direct+ && \
 source ./install/setup.bash && \
 colcon build --packages-select test_bench && \
+source ./install/setup.bash && \
+colcon build --packages-select marvelmind_ros2_msgs && \
+colcon build --packages-select marvelmind_ros2 && \
 source ./install/setup.bash
